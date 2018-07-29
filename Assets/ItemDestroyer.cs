0ab3d2735819726f23ad71f52cf8af8d295dc6c4
shoheiyamamoto <nitiham55@yahoo.co.jp>
2018-07-29 22:08:26 +0900

    StepAsideUnityChan
    基本・発展課題なし

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+
+    private GameObject unitychan;
+    private float offset = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        this.unitychan = GameObject.Find("unitychan");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(unitychan.transform.position.z);
+
+        if (this.transform.position.z + offset <= unitychan.transform.position.z)
+        { Destroy(this.gameObject);}
+	}
+}
